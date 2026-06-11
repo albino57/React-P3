caedcdbf7f6f3f79cbade2b126057f281f176966
Rafael Albino <ansdles@live.com>
2026-06-11 18:49:05 -0300

    feat: criado arquivo de style

---
+++ styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingTop: 60,
+    paddingHorizontal: 30,
+  },
+
+  textButtonLogin: {
+    color: "#ffffff",
+  },
+
+  buttonLogin: {
+    backgroundColor: "#288cff",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+    buttonLocalConnec: {
+    backgroundColor: "#84afff5d",
+    padding: 15,
+    borderRadius: 7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+})
