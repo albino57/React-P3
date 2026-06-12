c0673800a12af6bf2d9516ee7677f9f732b1a4c6
Rafael Albino <ansdles@live.com>
2026-06-12 12:26:45 -0300

    feat: adicionado várias tags e estilização de clonagem | Ainda em progresso

--- App.tsx
+++ App.tsx
@@ -1,18 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import {style} from "./styles"
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+import { style } from "./styles";
+import logo from "./assets/logoScreen.jpeg";
 
 export default function App() {
   return <View style={style.container}>
-    
+
+    {/*-----↓ LogoScreen ↓-----*/}
+    <Image style={style.image} source={logo} />
+    {/*-----↑ LogoScreen ↑-----*/}
+
+
+    {/*-----↓ View dos Inputs ↓-----*/}
+    <View style={style.View1}>
+      <Text style={style.textEmoje}>👤</Text>
+      <TextInput style={style.textInput} placeholder='Please enter Username or Email' />
+    </View>
+
+    <View style={style.lineView}></View>
+
+    <View style={style.View2}>
+      <Text style={style.textEmoje}>🔒</Text>
+      <TextInput style={style.textInput} placeholder='Please enter Password' />
+    </View>
+
+    <View style={style.lineView}></View>
+    {/*-----↑ View dos Inputs ↑-----*/}
+
+
+    {/*-----↓ Text ↓-----*/}
+    <Text style={style.textSwitchPhone}>Switch Phone Number Login</Text>
+    {/*-----↑ Text ↑-----*/}
+
 
     {/*-----↓ Botões ↓-----*/}
-    <TouchableOpacity onPress={()=>{}} activeOpacity={0.75} style={style.buttonLogin}>
+    <TouchableOpacity onPress={() => { }} activeOpacity={0.75} style={style.buttonLogin}>
       <Text style={style.textButtonLogin}>Login</Text>
     </TouchableOpacity>
 
-    <TouchableOpacity onPress={()=>{}} activeOpacity={0.75} style={style.buttonLocalConnec}>
-      <Text>Local Connection</Text>
+    <TouchableOpacity onPress={() => { }} activeOpacity={0.75} style={style.buttonLocalConnec}>
+      <Text style={style.textbuttonLocalConnec}>Local Connection</Text>
     </TouchableOpacity>
+    {/*-----↑ Botões ↑-----*/}
+
+
+    {/*-----↓ Painel Inferior Footer ↓-----*/}
+    <View style={style.ViewFooter}>
+      <Text style={style.textFooter}>Sign up now</Text>
+      <Text style={style.textFooter}>|</Text>
+      <Text style={style.textFooter2}>Forgot password</Text>
+    </View>
+    {/*-----↑ Painel Inferior Footer ↑-----*/}
   </View>
 }
--- styles.ts
+++ styles.ts
@@ -2,28 +2,88 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
     container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-    paddingTop: 60,
-    paddingHorizontal: 30,
-  },
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#ffffff",
+        paddingTop: 60,
+        paddingHorizontal: 30,
+    },
 
-  textButtonLogin: {
-    color: "#ffffff",
-  },
+    textButtonLogin: {
+        color: "#ffffff",
+        fontSize: 18,
+    },
+    textbuttonLocalConnec: {
+        color: "#288cff",
+        fontSize: 18,
+    },
+    textEmoje: {
+        paddingRight: 20,
+    },
+    textInput: {
+        fontSize: 16,
+    },
+    textSwitchPhone:{
+        color: "#288cff",
+        paddingTop: 7,
+        alignSelf: 'stretch',
+    },
+    textFooter:{
+        paddingRight: 25,
+        color: "#288cff",
+        fontSize: 16,
+    },
+    textFooter2:{
+        color: "#288cff",
+        fontSize: 16,
+    },
 
-  buttonLogin: {
-    backgroundColor: "#288cff",
-    padding: 15,
-    borderRadius: 7,
-    alignItems: "center",
-    marginTop: 20,
-  },
+    buttonLogin: {
+        backgroundColor: "#288cff",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+        marginTop: 20,
+        alignSelf: "stretch",
+    },
     buttonLocalConnec: {
-    backgroundColor: "#84afff5d",
-    padding: 15,
-    borderRadius: 7,
-    alignItems: "center",
-    marginTop: 20,
-  },
+        backgroundColor: "#84afff5d",
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#288cff",
+        alignItems: "center",
+        marginTop: 20,
+        alignSelf: "stretch",
+    },
+
+    image: {
+        resizeMode: 'contain',
+        width: 450,
+        height: 225,
+    },
+
+    lineView: {
+        height: 1,
+        backgroundColor: '#cecece58',
+        alignSelf: 'stretch',
+        marginTop: 10,
+    },
+
+    View1: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+    View2: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        marginTop: 30,
+    },
+    ViewFooter: {
+        flexDirection: 'row',
+        paddingTop: 45,
+    }
 })
